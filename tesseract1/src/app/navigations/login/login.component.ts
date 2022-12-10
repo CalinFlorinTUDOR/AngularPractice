c8a2f4a0e0e32a47dc24d6cb19f84511f1c92720
Calin Florin TUDOR <calinf.tudor@gmail.com>
2022-12-10 09:04:42 +0200

    register&login with validation stored both in LS

--- tesseract1/src/app/navigations/login/login.component.ts
+++ tesseract1/src/app/navigations/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  setForm!: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.setForm = new FormGroup ({
+
+      username: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+
+    });
   }
+
+  onSubmit() {
+
+    this.addMultipleUsers(this.setForm.value);
+  
+  }
+  
+    addMultipleUsers(user: any) {
+
+      if (this.setForm.valid) {
+      let users = [];
+      if (localStorage.getItem('Users')) {
+        users = JSON.parse(localStorage.getItem('Users' ) || '{}');
+        users = [user, ...users];
+
+        localStorage.setItem('Users', JSON.stringify(users));
+
+    } else {
+      users = [user];
+      
+      localStorage.setItem('Users', JSON.stringify(users));
+
+    }
+  }
+    
+    }
+
+  get username(): FormControl {
+      return this.setForm.get('username') as FormControl;
+    }  
+
+    get password(): FormControl {
+      return this.setForm.get('password') as FormControl;
+    }  
 
 }
--- tesseract1/src/app/navigations/register/register.component.ts
+++ tesseract1/src/app/navigations/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,23 +8,86 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   
-  myform!: any;
+  setForm!: any;
+  pass1: string = '';
+  pass2: string = '';
+  
   constructor() {}
 
   ngOnInit() {
-    this.myform = new FormGroup ({
-      username: new FormControl(''),
-      email: new FormControl(''),
-      fname: new FormControl(''),
-      lname: new FormControl(''),
-      password: new FormControl(''),
-      passwors2nd: new FormControl(''),
-      age: new FormControl('')
+    
+    this.setForm = new FormGroup ({
+
+      username: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      fname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      lname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      password2nd: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      age: new FormControl('', [Validators.required, Validators.min(18), Validators.max(130)])
+
     });
+
+    
+
   }
 
   onSubmit() {
 
-  localStorage.setItem('userData', JSON.stringify(this.myform.value))
+  console.log(this.setForm.value)
+
+  this.addMultipleUsers(this.setForm.value);
+
 }
+
+  addMultipleUsers(user: any) {
+
+    if (this.setForm.valid && this.pass1 === this.pass2) {
+      let users = [];
+    if (localStorage.getItem('Users')) {
+      users = JSON.parse(localStorage.getItem('Users' ) || '{}');
+      users = [user, ...users];
+
+      localStorage.setItem('Users', JSON.stringify(users));
+
+  } else {
+    users = [user];
+    
+    localStorage.setItem('Users', JSON.stringify(users));
+    }
+    }
+  
+  }
+
+  get username(): FormControl {
+    return this.setForm.get('username') as FormControl;
+    
+  }  
+
+  get email(): FormControl {
+    return this.setForm.get('email') as FormControl;
+  }  
+
+  get fname(): FormControl {
+    return this.setForm.get('fname') as FormControl;
+  }  
+
+  get lname(): FormControl {
+    return this.setForm.get('lname') as FormControl;
+  }  
+
+  get password(): FormControl {
+    return this.setForm.get('password') as FormControl;
+  } 
+  
+  get password2nd(): FormControl {
+    return this.setForm.get('password2nd') as FormControl;
+  } 
+
+  get age(): FormControl {
+    return this.setForm.get('age') as FormControl;
+  } 
+
+
+
 }
